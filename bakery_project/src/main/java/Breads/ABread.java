706f9ead248246f71ce1238f7ff5882114ad018b
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-04-20 15:41:45 +0200

    Abstract class in decorator
    I finished the abstract class in the decorator class and started to finish the inheritance in the child classes

--- bakery_project/src/main/java/Breads/ABread.java
+++ bakery_project/src/main/java/Breads/ABread.java
@@ -1,4 +1,21 @@
 package Breads;
 
+import java.util.List;
+
 public abstract class ABread {
+    private int id;
+    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
+
+    private String name;
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+
+    private int price;
+    public int getPrice() { return this.price; }
+    public void setPrice(int price) { this.price = price; }
+
+    List<String> ingredients;
+    public abstract List<String>getIngredients();
+
 }
--- bakery_project/src/main/java/Breads/FruitBread.java
+++ bakery_project/src/main/java/Breads/FruitBread.java
@@ -1,4 +1,27 @@
 package Breads;
 
-public class FruitBread {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitBread extends ABread {
+
+    List<String>fruits;
+    public List<String> getFruits() {
+        List<String> temp = new ArrayList<>();
+        temp.add("prunes");
+        temp.add("driend peaches");
+        temp.add("raisins");
+        return temp;
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        List<String>temp = new ArrayList<>();
+        temp.add("flour");
+        temp.add("egg");
+        temp.add("water");
+        temp.add("yeast");
+        temp.add("butter");
+        return temp;
+    }
 }
