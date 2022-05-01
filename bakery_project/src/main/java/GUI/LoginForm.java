4d3208505e11f968abd1c5430da166532108d73a
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-05-01 22:08:42 +0200

    Initialization of JDBC
    i initialized the JDBC to work properly. Tuturial needed to propelry set up the database in the workspace

--- bakery_project/src/main/java/GUI/LoginForm.java
+++ bakery_project/src/main/java/GUI/LoginForm.java
@@ -1,6 +1,12 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class LoginForm extends JFrame {
     private JLabel lblLogin;
@@ -17,5 +23,51 @@ public class LoginForm extends JFrame {
         setSize(350, 500);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try
+                {
+                    // create our mysql database connection
+                    String myDriver = "com.mysql.jdbc.Driver";
+                    String myUrl = "jdbc:mysql://localhost:3306/bakery";
+                    //Class.forName(myDriver);
+                    Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+                    // our SQL SELECT query.
+                    // if you only need a few columns, specify them by name instead of using "*"
+                    String query = "SELECT * FROM users";
+
+                    // create the java statement
+                    Statement st = conn.createStatement();
+
+                    // execute the query, and get a java resultset
+                    ResultSet rs = st.executeQuery(query);
+
+                    // iterate through the java resultset
+                    while (rs.next())
+                    {
+                        int id = rs.getInt("id");
+                        String FirstName = rs.getString("username");
+                        String Email = rs.getString("email");
+                        String Password = rs.getString("password");
+
+
+                        // print the results
+                        System.out.format("%s, %s, %s, %s\n", id, FirstName,Email,Password);
+                    }
+                    st.close();
+                }
+                catch (Exception ex)
+                {
+                    System.err.println("Got an exception! ");
+                    System.err.println(ex.getMessage());
+                }
+            }
+        });
     }
+
+
+
 }
