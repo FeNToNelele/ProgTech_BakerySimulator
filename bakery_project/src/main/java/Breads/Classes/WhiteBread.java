fed7c0bb11826013feda29ae5dfe301ece132ed7
FeNToNelele <sarosigab@gmail.com>
2022-05-01 19:16:55 +0200

    Code refactoring and implemented WhiteBread class

--- bakery_project/src/main/java/Breads/Classes/WhiteBread.java
+++ bakery_project/src/main/java/Breads/Classes/WhiteBread.java
@@ -1,4 +1,39 @@
 package Breads.Classes;
 
-public class WhiteBread {
+import Breads.ABread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WhiteBread extends ABread {
+    public WhiteBread(int id) {
+        super(id);
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        List<String> temp = new ArrayList<>();
+        temp.addAll(super.getIngredients());
+        temp.add(getExtraFlour());
+        return temp;
+    }
+
+    public String getExtraFlour() {
+        return "extra white flour mix";
+    }
+
+    @Override
+    public String getName() {
+        return "White bread";
+    }
+
+    @Override
+    public int getPrice() {
+        return 280;
+    }
+
+    @Override
+    public ABread clone() {
+        return new WhiteBread(this.getId());
+    }
 }
