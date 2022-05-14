9e15856263fd4dd1f5bd62ed572516597e069aff
FeNToNelele <sarosigab@gmail.com>
2022-05-14 18:34:24 +0200

    Implemented FileCreator class for SRP

---
+++ bakery_project/src/main/java/Storage/Classes/FileCreator.java
@@ -0,0 +1,32 @@
+package Storage.Classes;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreator {
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.setFilename(filename);
+    }
+
+    public FileCreator(String filename) {
+        this.filename = filename;
+    }
+
+    public void createFile() {
+        try {
+            File myObj = new File(this.getFilename());
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + this.getFilename());
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
