c558e3a37a1e0ae751a11e4be325f1226389a436
FeNToNelele <sarosigab@gmail.com>
2022-05-16 17:15:51 +0200

    Updated logging - Now also logging errors

--- bakery_project/src/main/java/Storage/Strategies/LogAdd.java
+++ bakery_project/src/main/java/Storage/Strategies/LogAdd.java
@@ -2,9 +2,14 @@ package Storage.Strategies;
 
 import Storage.Classes.FileCreator;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class LogAdd extends ALog {
     @Override
@@ -16,7 +21,45 @@ public class LogAdd extends ALog {
             myWriter.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
+            try {
+                fileCreator.setFilename("logError.txt");
+                FileWriter myWriter = new FileWriter(fileCreator.getFilename(), true);
+                myWriter.write(String.format("An error occoured while logging addition at %s\n", now = LocalDateTime.now()));
+                myWriter.close();
+            } catch (IOException ex) {
+                System.out.println("An error occurred.");
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
+
+        try {
+            String myDriver = "com.mysql.jdbc.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/bakery";
+            Connection conn = DriverManager.getConnection(myUrl, "root", "");
+            File logFile = new File("logAdd.txt");
+            Scanner logReader = new Scanner(logFile);
+            Statement st = conn.createStatement();
+            st.executeUpdate("DELETE from usagelog");
+            while (logReader.hasNextLine()) {
+                String[] data = logReader.nextLine().split(" ");
+                st.executeUpdate("INSERT INTO usagelog " +
+                        "VALUES ("+data[1]+", '"+data[5]+" "+data[6]+"', '"+data[8]+"')");
+            }
+            logReader.close();
+        }
+        catch (Exception ex){
+            System.out.println("An error occoured while exporting data to database.");
+            try {
+                fileCreator.setFilename("logError.txt");
+                FileWriter myWriter = new FileWriter(fileCreator.getFilename(), true);
+                myWriter.write(String.format("An error occoured while exporting data to database at %s\n", now = LocalDateTime.now()));
+                myWriter.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
     }
 }
