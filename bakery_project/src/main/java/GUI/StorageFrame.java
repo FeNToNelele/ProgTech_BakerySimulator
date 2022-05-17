339c1d026e84ccde663aa57ad1bb6e719a408343
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-05-17 20:26:29 +0200

    JTable update

--- bakery_project/src/main/java/GUI/StorageFrame.java
+++ bakery_project/src/main/java/GUI/StorageFrame.java
@@ -13,10 +13,14 @@ import Storage.Strategies.LogAdd;
 import Storage.Strategies.LogError;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.List;;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Stream;;
 import Storage.Strategies.LogRemove;
 
 public class StorageFrame extends JFrame {
@@ -116,28 +120,41 @@ public class StorageFrame extends JFrame {
     }
 
     private void initTable() {
+        DefaultTableModel defaultTableModel = (DefaultTableModel) tblData.getModel();
         tblData.setModel(new DefaultTableModel(
                 getStorageDataAsJaggedArray(),
                 new String[] {"Id", "Name", "Price"}
         ));
+        defaultTableModel.setRowCount(0);
     }
 
     private void refreshTable() {
-        DefaultTableModel defaultTableModel = (DefaultTableModel) tblData.getModel();
-        defaultTableModel.setRowCount(0);
         initTable();
     }
 
 
     private String[][] getStorageDataAsJaggedArray() {
+        String[][] dataFromTable = getTableData(tblData);
         String[][] data = new String[bakery.storage.getProducts().size()][];
         List<ABread> products = bakery.storage.getProducts();
-
         for (int i = 0; i < products.size(); i++) {
             ABread bread = products.get(i);
             data[i] = new String[]{ Integer.toString(bread.getId()), bread.getName(), Integer.toString(bread.getPrice()) };
         }
+        String[][] finData = Stream.concat( Arrays.stream( dataFromTable ), Arrays.stream( data ) ).toArray( String[][]::new );
+        return finData;
+    }
 
-        return data;
+
+    private String[][] getTableData (JTable table) {
+        TableModel dtm = table.getModel();
+        int nRow = dtm.getRowCount(), nCol = dtm.getColumnCount();
+        String[][] tableData = new String[nRow][nCol];
+        for (int i = 0 ; i < nRow ; i++) {
+            for (int j = 0; j < nCol; j++) {
+                tableData[i][j] = dtm.getValueAt(i, j).toString();
+            }
+        }
+        return tableData;
     }
 }
